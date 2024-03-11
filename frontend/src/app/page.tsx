57448bd5837028d842d8529ab294a25aeadc2a05
Zolzaya Udval <23LP6080@G301-CC0198.local>
2024-03-11 13:11:56 +0800

    edit

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,15 @@
+import { GeneralCard } from "@/components";
 import { Footer } from "@/components/Footer";
 import { CategoryBar } from "@/components/header/Category-Bar";
 import { Container } from "@/constants/Container";
 import Image from "next/image";
 
 export default function Home() {
+  return (
+    <div>
+      <GeneralCard />
+    </div>
+  );
   return (
     <div>
       <Footer />
