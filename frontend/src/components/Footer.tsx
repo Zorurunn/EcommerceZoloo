69146a48b16e5d5af0cf471e68d4c0c87d3e384f
Binderya Enkhtuvshin <binderyaenkhtuvshin@Binderyas-MacBook-Pro.local>
2024-03-11 12:41:19 +0800

    Footer edit

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -56,6 +56,7 @@ export function Footer() {
                             textUnderlinePosition: "under"
                         }}
                     >
+                        
                         <Link href="/"
                         >
                             Холбоо барих
