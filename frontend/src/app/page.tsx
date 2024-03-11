7cdf45e770a66b48de090b3894d0b995cf9ae0e7
Saikhanbayar Sainjargal <23LP6389@G301-CC0194.local>
2024-03-11 14:43:25 +0800

    categoryBar

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,13 @@
 import { Footer } from "@/components/Footer";
+import { CategoryBar } from "@/components/header/Category-Bar";
 import { Container } from "@/constants/Container";
 import Image from "next/image";
 
 export default function Home() {
-  return <div>
-    <Footer/>
-  </div>;
+  return (
+    <div>
+      <Footer />
+      <CategoryBar />
+    </div>
+  );
 }
