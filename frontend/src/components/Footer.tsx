4381ca78304a5ff25842df3eec6c408ac0844500
Binderya Enkhtuvshin <binderyaenkhtuvshin@Binderyas-MacBook-Pro.local>
2024-03-12 11:09:49 +0800

    Pretty

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,108 +1,90 @@
-"use client"
+"use client";
 
 import { Container, Stack, Typography } from "@mui/material";
 import { Poppins } from "next/font/google";
-import Link from "next/link"
-import { usePathname } from "next/navigation"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export function Footer() {
-    const pathname = usePathname();
+  const pathname = usePathname();
 
-    return (
-
-
+  return (
+    <Stack
+      sx={{
+        bgcolor: "#12A795",
+        py: "114px",
+      }}
+    >
+      <Container maxWidth="xl">
         <Stack
-            sx={{
-                bgcolor: "#12A795",
-                py: "114px"
-            }}
+          sx={{
+            gap: "48px",
+            alignItems: "center",
+            width: "100%",
+          }}
         >
-            <Container
-                maxWidth="xl"
+          <Stack
+            direction="row"
+            justifyContent={"center"}
+            alignItems="center"
+            gap="18px"
+          >
+            <img src="/Logo.png" alt="" />
+            <Typography
+              sx={{
+                color: "#fff",
+                fontSize: "20px",
+                fontWeight: "700",
+                lineHeight: "30px",
+              }}
             >
-                <Stack
-                    sx={{
-                        gap: "48px",
-                        alignItems: "center",
-                        width: "100%"
-
-                    }}
-                >
-                    <Stack
-                        direction="row"
-                        justifyContent={"center"}
-                        alignItems="center"
-                        gap="18px">
-                        <img src="/Logo.png" alt="" />
-                        <Typography
-                            sx={{
-                                color: "#fff",
-                                fontSize: "20px",
-                                fontWeight: "700",
-                                lineHeight: "30px"
-                            }} >
-                            Ecommerce
-                        </Typography>
-                    </Stack>
-                    <Stack direction="row"
-                        alignItems="center"
-                        justifyContent={"space-between"}
-                        color={"#fff"}
-                        style={{
-                            width: "100%",
-                            fontWeight: 590,
-                            fontSize: 16,
-                            textDecorationLine: "underline",
-                            textUnderlinePosition: "under"
-                        }}
-                    >
-                        
-                        <Link href="/"
-                        >
-                            Холбоо барих
-                        </Link>
-                        <Link href="/">
-                            Үйлчилгээний нөхцөл
-                        </Link>
-                        <Link href="/">
-                            Хүргэлтийн бүс
-                        </Link>
-                        <Link href="/" >
-                            Нууцлалын бодлого
-                        </Link>
-
-                    </Stack>
-                    <Stack
-                        direction="row"
-                        justifyContent="center"
-                        alignItems="center"
-                        width={"100%"}
-                        borderBottom={1} borderColor="#fff"
-                    >
-                    </Stack>
-                    <Stack direction="row"
-                        gap="18px">
-                        <img src="/facebook.png" alt="" />
-                        <img src="/instagram.png" alt="" />
-                        <img src="/twitter.png" alt="" />
-                    </Stack>
-                    <Stack
-                        color="#fff"
-                        gap="8px"
-                        fontWeight="400"
-                        fontSize="16px"
-                        lineHeight="20px"
-                        alignItems={"center"}
-                    >
-                        <Typography>© 2024 Pinecone Ecommmerce LLC </Typography>
-                        <Typography>Зохиогчийн эрх хуулиар хамгаалагдсан.</Typography>
-
-
-                    </Stack>
-                </Stack>
-            </Container>
+              Ecommerce
+            </Typography>
+          </Stack>
+          <Stack
+            direction="row"
+            alignItems="center"
+            justifyContent={"space-between"}
+            color={"#fff"}
+            style={{
+              width: "100%",
+              fontWeight: 590,
+              fontSize: 16,
+              textDecorationLine: "underline",
+              textUnderlinePosition: "under",
+            }}
+          >
+            <Link href="/">Холбоо барих</Link>
+            <Link href="/">Үйлчилгээний нөхцөл</Link>
+            <Link href="/">Хүргэлтийн бүс</Link>
+            <Link href="/">Нууцлалын бодлого</Link>
+          </Stack>
+          <Stack
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+            width={"100%"}
+            borderBottom={1}
+            borderColor="#fff"
+          ></Stack>
+          <Stack direction="row" gap="18px">
+            <img src="/facebook.png" alt="" />
+            <img src="/instagram.png" alt="" />
+            <img src="/twitter.png" alt="" />
+          </Stack>
+          <Stack
+            color="#fff"
+            gap="8px"
+            fontWeight="400"
+            fontSize="16px"
+            lineHeight="20px"
+            alignItems={"center"}
+          >
+            <Typography>© 2024 Pinecone Ecommmerce LLC </Typography>
+            <Typography>Зохиогчийн эрх хуулиар хамгаалагдсан.</Typography>
+          </Stack>
         </Stack>
-
-
-    );
+      </Container>
+    </Stack>
+  );
 }
