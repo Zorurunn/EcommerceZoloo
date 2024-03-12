381f4ea5a53d5c39c70611bfac015c5603eea2e5
Baljinnyam Buyanjargal <23LP8560@G301-CC0203-2.local>
2024-03-12 11:52:45 +0800

    page ustgav

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,3 @@
-import { CardFormStep1 } from "@/components/CardFormStep1";
-import { Footer } from "@/components/Footer";
-import { CategoryBar } from "@/components/header/Category-Bar";
-import Navbar from "@/components/header/Navbar";
-import { Stack } from "@mui/material";
-import { Container } from "@mui/material";
-import { CardFormStep2 } from "./signup/_components/CardFormStep2";
-import SignupForm from "./signup/_components/SignupForm";
-import { CardFormStep3 } from "./signup/_components/CardFormStep3";
-
 export default function Home() {
-  return (
-    <Stack>
-      <Navbar />
-      <Container maxWidth="xl">
-        <CategoryBar />
-        <CardFormStep1 />
-        <CardFormStep2 />
-        <SignupForm />
-      </Container>
-      <Footer />
-      <CardFormStep3 />
-    </Stack>
-  );
+  return <></>;
 }
--- frontend/src/constants/index.ts
+++ frontend/src/constants/index.ts
@@ -1,4 +1,4 @@
-// FOOTER SECTION
+// CardStep3
 export const CardFormStep3Experience = ["Сонгох", "2jil", "3jil"];
 
 export const CardFormStep3Products = ["Сонгох", "2jil", "3jil"];
