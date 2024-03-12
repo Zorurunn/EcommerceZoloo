8772b4485c757c5c69990a395bcaa262430554df
Zolzaya Udval <23LP6080@G301-CC0198.local>
2024-03-12 12:26:27 +0800

    api

---
+++ api/src/routers/auth.router.ts
@@ -0,0 +1,19 @@
+// import { Router } from "express";
+// import {
+//   signIn,
+//   signUp,
+//   reNewPassword,
+//   otpGenerate,
+//   changePassword,
+// } from "../controllers/auth.controller";
+
+// const authRouter = Router();
+
+// authRouter
+//   .post("/signIn", signIn)
+//   .post("/signup", signUp)
+//   .post("/reNewPassword", reNewPassword)
+//   .post("/otpGenerate", otpGenerate)
+//   .post("/changePassword", changePassword);
+
+// export default authRouter;
