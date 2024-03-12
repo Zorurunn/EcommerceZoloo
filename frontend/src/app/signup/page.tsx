4f623254c4036dc7139371fe857b29895fc7b904
Saikhanbayar Sainjargal <23LP6389@G301-CC0194.local>
2024-03-12 13:36:50 +0800

    kk

--- frontend/src/app/signup/page.tsx
+++ frontend/src/app/signup/page.tsx
@@ -3,12 +3,13 @@
 import { Stack, Typography } from "@mui/material";
 import Image from "next/image";
 import { useState } from "react";
-import SignupForm from "./_components/SignupForm";
 import { CardFormStep2 } from "./_components/CardFormStep2";
 import { CardFormStep3 } from "./_components/CardFormStep3";
 import { CardFormStep1 } from "./_components/CardFormStep1";
+import SignInForm from "../signin/_components/SignInForm";
+import SignUpForm from "./_components/SignUpForm";
 
-export default function Signup() {
+export default function SignUp() {
   const [step, setStep] = useState(1);
 
   return (
@@ -22,12 +23,12 @@ export default function Signup() {
       <Stack position={"relative"} px={"6%"} py={"2%"} alignSelf={"flex-start"}>
         <Image src="/Pinecone Logo.svg" alt="" fill />
       </Stack>
-      {step === 1 && <SignupForm setStep={setStep} />}
+      {step === 1 && <SignUpForm setStep={setStep} />}
       {step === 2 && <CardFormStep1 setStep={setStep} />}
       {step === 3 && <CardFormStep2 setStep={setStep} />}
       {step === 4 && <CardFormStep3 setStep={setStep} />}
 
-      <Stack>
+      <Stack marginTop={8}>
         <Typography color={"#94A3B1"}>© 2023 Pinecone</Typography>
       </Stack>
     </Stack>
