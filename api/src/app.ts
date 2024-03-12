92262dd47dcc815d90904ee764478bc7ba3d7611
Zolzaya Udval <23LP6080@G301-CC0198.local>
2024-03-12 12:56:24 +0800

    database

--- api/src/app.ts
+++ api/src/app.ts
@@ -1,31 +1,16 @@
-// import express from "express";
-// import cors from "cors";
-// import { json } from "body-parser";
-// import authRouter from "./routers/auth.router";
-// import foodRouter from "./routers/food.router";
-// import { authMiddleware } from "./middlewares";
-// import emailRouter from "./routers/email.router";
-// import getUserRouter from "./routers/getUser.router";
-// import userUpdateRouter from "./routers/userUpdate.router";
-// import getRouter from "./routers/get.router";
-// import basketRouter from "./routers/basket.router";
-// import orderRouter from "./routers/order.router";
+import express from "express";
+import cors from "cors";
+import { json } from "body-parser";
+import authRouter from "./routers/auth.router";
+import { authMiddleware } from "./middlewares/auth.middleware";
 
-// const app = express();
+const app = express();
 
-// app.use(cors());
-// app.use(json());
+app.use(cors());
+app.use(json());
 
-// app.use("/", authRouter);
-// app.use("/", emailRouter);
-// app.use("/", foodRouter);
-// app.use("/", orderRouter);
-// app.use("/", getRouter);
+app.use("/", authRouter);
 
-// app.use(authMiddleware);
+app.use(authMiddleware);
 
-// app.use("/", getUserRouter);
-// app.use("/", userUpdateRouter);
-// app.use("/", basketRouter);
-
-// export default app;
+export default app;
