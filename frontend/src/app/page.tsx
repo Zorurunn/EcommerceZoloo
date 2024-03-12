89a4bbf989e2b2804a74a9df431b6c3caac2673f
Binderya Enkhtuvshin <binderyaenkhtuvshin@Binderyas-MacBook-Pro.local>
2024-03-12 12:09:55 +0800

    BlackHeader

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,25 @@
+import { CardFormStep1 } from "@/components/CardFormStep1";
+import { Footer } from "@/components/Footer";
+import { CategoryBar } from "@/components/header/Category-Bar";
+import Navbar from "@/components/header/Navbar";
+import { Stack } from "@mui/material";
+import { Container } from "@mui/material";
+import { CardFormStep2 } from "./signup/_components/CardFormStep2";
+import SignupForm from "./signup/_components/SignupForm";
+import { BlackHeader } from "@/components/header/BlackHeader";
+
 export default function Home() {
-  return <></>;
+  return (
+    <Stack>
+      <Navbar />
+      <BlackHeader />
+      <Container>
+        <CategoryBar />
+        <CardFormStep1 />
+        <CardFormStep2 />
+        <SignupForm />
+      </Container>
+      <Footer />
+    </Stack>
+  );
 }
