b638ae9491dc46b5a7f134b3c191a186d7645b66
Baljinnyam Buyanjargal <23LP8560@G301-CC0203-2.local>
2024-03-12 09:41:47 +0800

    delete

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,5 @@
 import { Footer } from "@/components/Footer";
 import { CategoryBar } from "@/components/header/Category-Bar";
-import SignupForm from "@/app/signup/_components/SignupForm";
-import { Container } from "@/constants/Container";
-import Image from "next/image";
 
 export default function Home() {
   return (
