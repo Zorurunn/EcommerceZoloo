8c7e20b4083ced195c28d805c984f40328992151
ulaaka211 <uulaaka73gmail.com>
2024-03-19 10:00:14 +0800

    kk

--- frontend/src/app/dashboard/product/page.tsx
+++ frontend/src/app/dashboard/product/page.tsx
@@ -1,9 +1,11 @@
+import { SettingsPage } from "../settings/_components/SettingsPage";
 import { ProductAllInfo } from "./_components/ProductAllInfo";
 
 export default function Home() {
   return (
     <>
-      <ProductAllInfo />
+      {/* <ProductAllInfo /> */}
+      <SettingsPage />
     </>
   );
 }
