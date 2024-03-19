8199d298f9a78c74f279babdb05a0bfd4b985694
Baljinnyam Buyanjargal <23LP8560@G302-CC0162.local>
2024-03-19 19:14:48 +0800

    ls

--- frontend/src/app/dashboard/income/_components/test.tsx
+++ frontend/src/app/dashboard/income/_components/test.tsx
@@ -14,7 +14,7 @@ const style = {
   width:"100%",
   position: 'absolute' as 'absolute',
   top: '50%',
-  left: '50%',
+//   left: '50%',
   transform: 'translate(-50%, -50%)',
   bgcolor: "#ffff",
   borderRadius: 6,
