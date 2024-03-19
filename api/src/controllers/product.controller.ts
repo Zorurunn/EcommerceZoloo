86ee7a723e4799b47af247aa8a552b3ffc3368db
Zolzaya Udval <23LP6080@G301-CC0198.local>
2024-03-19 08:24:19 +0800

    a

--- api/src/controllers/product.controller.ts
+++ api/src/controllers/product.controller.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import jwt from "jsonwebtoken";
 import { ProductModel } from "../models";
+import { ViewModel } from "../models/view.model";
 
 // CREATE PRODUCT
 export const createProduct: RequestHandler = async (req, res) => {
@@ -38,6 +39,37 @@ export const createProduct: RequestHandler = async (req, res) => {
     // });
 
     return res.json({ message: "Шинэ хэрэглэгч амжилттай үүслээ" });
+  } catch (error) {
+    res.json(error);
+  }
+};
+
+// VIEW UPDATE
+export const viewUpdate: RequestHandler = async (req, res) => {
+  const { userId, productId } = req.body;
+
+  try {
+    const isViewed = await ViewModel.find({ userId, productId });
+
+    if (isViewed.length) {
+      const newViewQty = isViewed[0].viewQty + 1;
+      await ViewModel.updateOne(
+        {
+          userId,
+          productId,
+        },
+        { viewQty: newViewQty }
+      );
+      return res.json({ message: "View quantity updated" });
+    }
+
+    await ViewModel.create({
+      userId,
+      productId,
+      viewQty: 1,
+    });
+
+    return res.json({ message: "View quantity created" });
   } catch (error) {
     res.json(error);
   }
