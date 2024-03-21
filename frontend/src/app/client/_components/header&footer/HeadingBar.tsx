5e424a9654f0799010c4e89a76e29b892c5bab8a
Baljinnyam Buyanjargal <23LP8560@G301-CC0203-2.local>
2024-03-21 14:18:09 +0800

    header footer

---
+++ frontend/src/app/client/_components/header&footer/HeadingBar.tsx
@@ -0,0 +1,81 @@
+"use client";
+import { Stack, Container, Typography, Avatar } from "@mui/material";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
+import MailOutlineIcon from "@mui/icons-material/MailOutline";
+import PhoneInTalkOutlinedIcon from "@mui/icons-material/PhoneInTalkOutlined";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import React from "react";
+
+export const HeadingBar = () => {
+  return (
+    <Stack position={"sticky"} width="100vw" bgcolor="#7E33E0">
+      <Container maxWidth="xl">
+        <Stack
+          direction={"row"}
+          justifyContent={"space-between"}
+          padding={"10px"}
+          alignItems={"center"}
+        >
+          <Stack direction={"row"} gap={8} alignItems={"center"}>
+            <Stack
+              direction={"row"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              gap={2}
+              color={"#F1F1F1"}
+            >
+              <MailOutlineIcon sx={{ fontSize: "18px" }} />
+              <Typography fontSize={16} fontWeight={600}>
+                info@ecommerce.mn
+              </Typography>
+            </Stack>
+            <Stack
+              direction={"row"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              gap={2}
+              color={"#F1F1F1"}
+            >
+              <PhoneInTalkOutlinedIcon sx={{ fontSize: "18px" }} />
+              <Typography fontSize={16} fontWeight={600}>
+                77123456
+              </Typography>
+            </Stack>
+          </Stack>
+          <Stack
+            direction={"row"}
+            alignItems={"center"}
+            gap={3}
+            color={"#F1F1F1"}
+          >
+            <Stack
+              direction={"row"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              gap={0.5}
+            >
+              <Typography fontSize={16} fontWeight={600}>
+                Нэвтрэх
+              </Typography>
+              <PersonOutlineIcon sx={{ fontSize: "18px" }} />
+            </Stack>
+            <Stack
+              direction={"row"}
+              alignItems={"center"}
+              justifyContent={"center"}
+              gap={0.5}
+            >
+              <Typography fontSize={16} fontWeight={600}>
+                Хадгалах
+              </Typography>
+              <FavoriteBorderIcon sx={{ fontSize: "18px" }} />
+            </Stack>
+            <ShoppingCartIcon sx={{ fontSize: "22px" }} />
+          </Stack>
+        </Stack>
+      </Container>
+    </Stack>
+  );
+};
--- frontend/src/app/client/layout.tsx
+++ frontend/src/app/client/layout.tsx
@@ -6,8 +6,9 @@ import { CssBaseline, ThemeProvider } from "@mui/material";
 import { ToastContainer } from "react-toastify";
 import { theme } from "@/theme";
 import { DataProvider } from "@/components/provider/DataProvider";
-import Navbar from "./_components/header&footer/Navbar";
+import Navbar from "./_components/header&footer/HeadingBar";
 import { Footer } from "./_components/header&footer/Footer";
+import Header from "./_components/header&footer/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -27,7 +28,7 @@ export default function RootLayout({
           <ThemeProvider theme={theme}>
             <AuthProvider>
               <DataProvider>
-                <Navbar />
+                <Header />
                 {children}
               </DataProvider>
               <Footer />
